f1b3b24166b13fc5c204e846aa99c1778d9d4738
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-01-25 17:07:11 +0900

    DIP/OCP 위반

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
   private final MemberRepository memberRepository = new MemoryMemberRepository();
-  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+  // 추상(인터페이스) (DiscountPolicy) 뿐만 아니라 구체(구현) 클래스(RateDiscountPolicy, FixDiscountPolicy) 에도 의존하고 있다.
+  // 지금 코드는 기능을 확장해서 변경하면, 클라이언트 코드에 영향을 준다! 따라서 OCP를 위반한다.
+  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
